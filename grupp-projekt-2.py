691a2663ef9f02980609ed7a05b84857fc93452c
kevin3169 <ardaalagoz51@gmail.com>
2025-10-01 15:09:57 +0200

    test commit i# with '#' will be ignored, and an empty message aborts the commit.

---
+++ grupp-projekt-2.py
@@ -0,0 +1,68 @@
+li1 =[
+  {
+    "question 1": "Vilket land har vunnit flest VM-guld i fotboll (herr)?",
+    "options": ["A) Brasilien",
+                "B) Tyskland",
+                "C) Italien"],
+    "answer": "A"
+  },
+  {
+    "question 2": "Hur många spelare finns på plan i ett basketlag samtidigt (per lag)?",
+    "options": ["A) 5",
+                "B) 6",
+                 "C) 7"],
+    "answer": "A"
+  },
+  {
+    "question 3": "Vilken svensk tennisspelare vann Wimbledon fem år i rad under 1970-talet?",
+    "options": ["A) "
+    "Stefan Edberg", "B) Björn Borg", 
+    "C) Mats Wilander"],
+    "answer": "B"
+  },
+  {
+    "question 4": "Vilken sport kallas ibland 'den vita sporten'?",
+    "options": ["A) Tennis",
+                "B) Golf",
+                "C) Cricket"],
+    "answer": "A"
+  },
+  {
+    "question 5": "I vilken idrott kan man vinna Stanley Cup?",
+    "options": ["A) Ishockey",
+                "B) Fotboll", 
+                "C) Baseball"],
+    "answer": "A"
+  },
+  {
+    "question 6": "Vilket år arrangerade Sverige sommar-OS i Stockholm?",
+    "options": ["A) 1912",
+                "B) 1932",
+                "C) 1956"],
+    "answer": "A"
+  },
+  {
+    "question 7": "Hur lång är en maratonlöpning?",
+    "options": ["A) 32km",
+                "B) 40,km",
+                "C) 42,km"],
+    "answer": "C"
+  },
+  {
+    "question 8": "Vilken svensk längdskidåkare kallas ofta 'Gunde'?",
+    "options": ["A) Gunde Svan", "B) Thomas Wassberg", "C) Sixten Jernberg"],
+    "answer": "A"
+  },
+  {
+    "question 9": "Vilken sport utövar man i Wimbledon?",
+    "options": ["A) Rugby", "B) Tennis", "C) Cykling"],
+    "answer": "B"
+  },
+  {
+    "question 10": "Vilket djur är symbol för den spanska sporten tjurfäktning?",
+    "options": ["A) Häst", "B) Tjur", "C) Hund"],
+    "answer": "B"
+  }]
+for element in li1:
+    print(element)
+
