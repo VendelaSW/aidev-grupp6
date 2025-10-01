cfa85cd0046c39fd5c2a9f7588c8ecd5c4d6c21c
Spirit Rosenberg <rosenberg.spirit@gmail.com>
2025-10-01 21:45:04 +0200

    Spirit: La in en klass och första frågan samt 2 variabler

---
+++ psykologi-frågor.py
@@ -0,0 +1,28 @@
+
+
+# import från pythons egna bibliotek för att kunna slumpa frågorna senare
+import random
+
+# Behöver en class
+class PsychologyQuestions:
+    def __init__(self):
+        self.score = 0
+        self.questions = [
+            {
+                "question": "Vilka delar av människan menade Freud att samhället förtrycker?",
+                "options": ["Sexualitet och aggressivitet", "Självbild", "Hungerkänslor", "Drömmar"],
+                "answer": 0
+
+
+
+            }
+
+
+
+
+        ]
+
+# Behöver sen lägga till variables med olika datatypes
+# Behöver även functions/methods - Hur sätter jag upp dessa?
+# Behöver även nån form av conditions (if(elif/else) och ev loop
+
